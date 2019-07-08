88ece465c72d1e6cad06a505b23174a0764fe21b
Weverson Cachinsky <weverson.cachinsky@lybe.se>
2019-07-08 18:13:48 -0300

    Add identity for further FPC flush

--- Block/Banner.php
+++ Block/Banner.php
@@ -4,10 +4,11 @@ namespace Weverson83\Banner\Block;
 use Weverson83\Banner\Api\BannerRepositoryInterface;
 use Weverson83\Banner\Api\Data\BannerInterface;
 
+use Magento\Framework\DataObject\IdentityInterface;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\View\Element\Template;
 
-class Banner extends Template
+class Banner extends Template implements IdentityInterface
 {
     /**
      * @var BannerRepositoryInterface
@@ -17,6 +18,10 @@ class Banner extends Template
      * @var SearchCriteriaBuilder
      */
     private $searchCriteriaBuilder;
+    /**
+     * @var BannerInterface
+     */
+    private $banner;
 
     public function __construct(
         BannerRepositoryInterface $bannerRepository,
@@ -29,16 +34,41 @@ class Banner extends Template
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
     }
 
+    /**
+     * Retrieves the first active banner
+     *
+     * @return BannerInterface|null
+     */
     public function getActiveBanner(): ?BannerInterface
     {
-        $this->searchCriteriaBuilder->addFilter(BannerInterface::ENABLED, 1);
-        $this->searchCriteriaBuilder->setPageSize(1);
-        $bannerList = $this->bannerRepository->getList($this->searchCriteriaBuilder->create());
+        if (!$this->banner) {
+            $this->searchCriteriaBuilder->addFilter(BannerInterface::ENABLED, 1);
+            $this->searchCriteriaBuilder->setPageSize(1);
+            $bannerList = $this->bannerRepository->getList($this->searchCriteriaBuilder->create());
 
-        foreach ($bannerList->getItems() as $banner) {
-            return $banner;
+            foreach ($bannerList->getItems() as $banner) {
+                $this->banner = $banner;
+
+                return $this->banner;
+            }
         }
 
         return null;
+    }
+
+    /**
+     * Return unique ID(s) for each object in system
+     *
+     * @return string[]
+     */
+    public function getIdentities()
+    {
+        $banner = $this->getActiveBanner();
+
+        if ($banner) {
+            return ['banner_id_' . $banner->getId()];
+        }
+
+        return ['banner_no_banner'];
     }
 }
